9f9488cd1c56e232d144956c2dde7bb9824d3de9
katerinab <katerina.b-p@hotmail.com>
2017-10-16 16:32:58 +1100

    finished code

--- implementation.py
+++ implementation.py
@@ -3,13 +3,15 @@ import numpy as np
 import glob #this will be useful when reading reviews from file
 import os
 import tarfile
+import re
+import string
 
 
 batch_size = 50
-lstmUnits = 64
+acc = 64
 numClasses = 2
-maxSeqLength = 10 #Maximum length of sentence
-numDimensions = 300 #Dimensions for each word vector
+maxSeqLength = 40 #Maximum length of sentence
+numDimensions = 50 #Dimensions for each word vector
 
 
 def extract_data(filename):
@@ -22,7 +24,6 @@ def extract_data(filename):
 
 def read_data():
     print("READING DATA")
-    data = []
     dir = os.path.dirname(__file__)
     file_list = glob.glob(os.path.join(dir, 'reviews/pos/*'))
     file_list.extend(glob.glob(os.path.join(dir, 'reviews/neg/*')))
@@ -38,13 +39,47 @@ def load_data(glove_dict):
     RETURN: numpy array of data with each row being a review in vectorized
     form"""
     extract_data('reviews.tar.gz')
-    data = read_data()
+    reviewData = read_data()
+    reviewArray = np.zeros(shape=(25000,40), dtype=int)
+    index = 0
+
+    for rev in reviewData:
+        with open(rev, "r", encoding='utf-8') as fileinput:
+            reviewText = ""
+            for line in fileinput:
+                line = line.rstrip().lower()
+                line = re.sub("<br />",' ',line)
+                line = re.sub('[%s]' % string.punctuation,'',line)
+                reviewText = reviewText + " " + line
+                #print(line)
+            #print ("Review: " + reviewText + "\n")
+            npArray = np.array(reviewText.split(' '))
+            npArray = list(filter(None, npArray))
+            npArray = np.asarray(npArray)
+            if len(npArray) > maxSeqLength:
+                npArray = npArray[:maxSeqLength]
+            #print(npArray)
+            for n,i in enumerate(npArray):
+                if i in glove_dict:
+                    npArray[n]=glove_dict[i]
+                else:
+                    npArray[n] = "0"
+            npArray = npArray.astype(int)
+            if len(npArray) != maxSeqLength:
+                npArray = np.pad(npArray, pad_width=(0, maxSeqLength-len(npArray)), mode='constant')
+            #print(npArray)
+            reviewArray[index] = npArray
+            index = index + 1
+
+    print('files finished')
+    print(reviewArray[5])
+    data = reviewArray
     return data
 
 def load_glove_embeddings():
     """
     Load the glove embeddings into a array and a dictionary with words as
-    keys and their associated index as the value. Assumes the glove
+    keys and their associated index as the val. Assumes the glove
     embeddings are located in the same directory and named "glove.6B.50d.txt"
     RETURN: embeddings: the array containing word vectors
             word_index_dict: a dictionary matching a word in string form to
@@ -55,11 +90,12 @@ def load_glove_embeddings():
     lines = data.readlines()
     #if you are running on the CSE machines, you can load the glove data from here
     #data = open("/home/cs9444/public_html/17s2/hw2/glove.6B.50d.txt",'r',encoding="utf-8")
-    i = 0
+    i = 1
     lineCount = len(lines)
     print(lineCount)
-    embeddings = np.zeros(shape=(lineCount,50))
+    embeddings = np.zeros(shape=(lineCount,50), dtype=np.float32)
     word_index_dict = {}
+    word_index_dict["UNK"] = 0
 
     for line in lines:
         lineSplit = line.split(' ', 1)
@@ -68,7 +104,7 @@ def load_glove_embeddings():
 
         npArray = np.fromstring(line, dtype=float, sep=' ')
 
-        embeddings[i] = npArray
+        embeddings[i-1] = npArray
 
         word_index_dict[word] = i
         i = i + 1
@@ -92,29 +128,29 @@ def define_graph(glove_embeddings_arr):
 
     RETURN: input placeholder, labels placeholder, dropout_keep_prob, optimizer, accuracy and loss
     tensors"""
-    lstm_units = 64
+    lstm_units = 32
+    dropoutRate = 0.6
 
-    labels = tf.placeholder(name="labels", dtype=tf.float32, shape=[batch_size,2])
-    input_data = tf.placeholder(name="input_data", dtype=tf.int32, shape=[batch_size,40])
-    dropout_keep_prob = tf.placeholder_with_default(0.75, name="dropout_keep_prob", shape=())
+    labels = tf.placeholder(name="labels", dtype=tf.float32, shape=[batch_size,numClasses])
+    input_data = tf.placeholder(name="input_data", dtype=tf.int32, shape=[batch_size,maxSeqLength])
+    embedData = tf.nn.embedding_lookup(glove_embeddings_arr, input_data)
 
-    data = tf.nn.embedding_lookup(glove_embeddings_arr, input_data)
+    dropout_keep_prob = tf.placeholder_with_default(dropoutRate, name="dropout_keep_prob", shape=())
 
     lstm = tf.contrib.rnn.BasicLSTMCell(lstm_units)
     lstm = tf.contrib.rnn.DropoutWrapper(cell=lstm, output_keep_prob=dropout_keep_prob)
-    value, _ = tf.nn.dynamic_rnn(lstm, data, dtype=tf.float32)
+    val, _ = tf.nn.dynamic_rnn(lstm, embedData, dtype=tf.float32)
 
-    weight = tf.Variable(tf.truncated_normal([lstm_units, 2]))
-    bias = tf.Variable(tf.constant(0.1, shape=[2]))
-    value = tf.transpose(value, [1, 0, 2])
-    last = tf.gather(value, int(value.get_shape()[0]) - 1)
-    prediction = (tf.matmul(last, weight) + bias)
+    w = tf.Variable(tf.truncated_normal([lstm_units, 2]))
+    b = tf.Variable(tf.constant(0.1, shape=[2]))
+    val = tf.transpose(val, [1, 0, 2])
+    last = tf.gather(val, int(val.get_shape()[0]) - 1)
+    pred = (tf.matmul(last, w) + b)
 
-    correctPred = tf.equal(tf.argmax(prediction,1), tf.argmax(labels,1))
+    correctPred = tf.equal(tf.argmax(pred,1), tf.argmax(labels,1))
     accuracy = tf.reduce_mean(tf.cast(correctPred, dtype=tf.float32))
 
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=labels))
-    print(loss)
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=labels))
     optimizer = tf.train.AdamOptimizer().minimize(loss)
 
     accuracy = tf.identity(accuracy, name="accuracy")
